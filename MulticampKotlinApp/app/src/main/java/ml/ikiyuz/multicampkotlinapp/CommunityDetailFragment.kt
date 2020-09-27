1c08e56ee1a01df73f4fd078ac70f140e782dc49
osmannyildiz <iamosmannyildiz@gmail.com>
2020-09-28 01:56:06 +0300

    Navigation and design

---
+++ MulticampKotlinApp/app/src/main/java/ml/ikiyuz/multicampkotlinapp/CommunityDetailFragment.kt
@@ -0,0 +1,11 @@
+package ml.ikiyuz.multicampkotlinapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CommunityDetailFragment : Fragment(R.layout.fragment_community_detail) {
+
+}
