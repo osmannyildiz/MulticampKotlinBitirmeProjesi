c1469327d24e6509e8b5f8bd57237040c2412ce8
osmannyildiz <iamosmannyildiz@gmail.com>
2020-09-27 23:37:50 +0300

    Create project and add dependencies

---
+++ MulticampKotlinApp/app/src/main/java/ml/ikiyuz/multicampkotlinapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ml.ikiyuz.multicampkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
